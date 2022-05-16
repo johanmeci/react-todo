1d60a598a0f3572ceedaec712d13337e1f2501fd
Johan Meneses <johanmeci03@gmail.com>
2022-05-16 16:30:01 -0500

    First commit

---
+++ src/components/TodoApp.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+export default function TodoApp() {
+
+  const [title, setTitle] = useState("");
+  const [todos, setTodos] = useState([]);
+  
+
+  function handleChange(e) {
+    const value = e.target.value;
+    setTitle(value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newTodo = {
+      id: crypto.randomUUID(),
+      title: title,
+      completed: false
+    }
+
+    const temp = [...todos];
+    temp.unshift(newTodo);
+
+    setTodos(temp);
+  }
+
+  return (
+    <div className="todoContainer">
+      <form className="todoCreateForm" onSubmit={handleSubmit}>
+        <input 
+          onChange={handleChange} 
+          className="todoInput" 
+          type="text" 
+          value={title} 
+        />
+
+        <input 
+          onClick={handleSubmit} 
+          className="buttonCreate" 
+          type="submit" 
+          value="Create Todo" 
+        />
+      </form>
+
+      <div className="todosContainer">
+        {
+          todos.map(item => (
+            <div key={item.id}>{item.title}</div>
+          ))
+        }
+      </div>
+    </div>
+  );
+}
